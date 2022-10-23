805db9b10ae053646b54be5ba23a1f9db78f099f
kohei0409 <kohei0409@mac.com>
2022-10-23 21:51:06 +0900

    multiple state update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment2 = () => setCount((previosCount) => previosCount + 1);
-  const decrement2 = () => setCount((previosCount) => previosCount - 1);
-  const resetButton = () => setCount(0);
-  const doubleButton = () => setCount((previosCount) => previosCount * 2);
-  const slitButton = () =>
-    setCount((previosCount) =>
-      previosCount % 3 === 0 ? previosCount / 3 : previosCount
-    );
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={resetButton}>RESET</button>
-        <button onClick={doubleButton}>✕２</button>
-        <button onClick={slitButton}>✕3の倍数だけ３で割る</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "サンプル",
+  price: 1000,
 };
 
 export default App;
