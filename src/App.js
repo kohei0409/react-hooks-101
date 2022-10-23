b3425aeef1cd23234e8616f17f9eb4b5221c4770
kohei0409 <kohei0409@mac.com>
2022-10-23 21:27:50 +0900

    my first react hooks with count

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is First React APP.</div>;
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount((previosCount) => previosCount + 1);
+  const decrement2 = () => setCount((previosCount) => previosCount - 1);
+  const resetButton = () => setCount(0);
+  const doubleButton = () => setCount((previosCount) => previosCount * 2);
+  const slitButton = () =>
+    setCount((previosCount) =>
+      previosCount % 3 === 0 ? previosCount / 3 : previosCount
+    );
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={resetButton}>RESET</button>
+        <button onClick={doubleButton}>✕２</button>
+        <button onClick={slitButton}>✕3の倍数だけ３で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
